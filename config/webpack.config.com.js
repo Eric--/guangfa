ea0f40528e644f367550b964f66052b6223245a3
erayliu <rmb5241@gmail.com>
2017-04-27 23:38:08 +0800

    repair question

--- config/webpack.config.com.js
+++ config/webpack.config.com.js
@@ -7,6 +7,10 @@ let ExtractTextPlugin = require('extract-text-webpack-plugin');
 let BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 let helpers = require('./helpers');
 
+let precss = require('precss');
+let postcssSprites = require('postcss-sprites');
+let autoprefixer = require('autoprefixer');
+
 module.exports = {
 
     module: {
@@ -19,15 +23,11 @@ module.exports = {
         }, {
             test: /\.css$/,
             include: helpers.root('src', 'app'),
-            use: ['to-string-loader', 'css-loader?sourceMap']
+            use: ['to-string-loader', 'css-loader', 'postcss-loader']
         }, {
             //.scss 文件使用 style-loader、css-loader 和 sass-loader 来编译处理
             test: /\.scss$/,
             loader: 'style-loader!css-loader!sass-loader?sourceMap'
-        }, {
-            //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
-            test: /\.(png|jpg)$/,
-            loader: 'url-loader?limit=8192'
         }, {
             //.js jsx 文件使用 babel-loader 来编译处理
             test: /\.js|jsx$/,
@@ -39,6 +39,9 @@ module.exports = {
             // support for .html as raw text
             test: /\.html$/,
             loader: 'raw-loader'
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: 'file-loader?name=/[hash:8].[name].[ext]'
         }]
     },
     //插件
@@ -63,11 +66,29 @@ module.exports = {
           inject: true
         }),
 
-        new BundleAnalyzerPlugin()
+        new BundleAnalyzerPlugin(),
+
+        //自定义插件
+        new webpack.LoaderOptionsPlugin({
+          options: {
+                //postcss
+                postcss: [
+                    autoprefixer,
+                    precss,
+                    postcssSprites({
+                        stylesheetPath: helpers.root('src/app/navbar'),
+                        spritePath: 'dist/css/images',
+                        retina: true, 
+                        padding: 4
+                    })
+                ] 
+          }
+        })
     ],
     //其它解决方案配置
     resolve: {
         //自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
         extensions: ['.ts', '.js', 'jsx', '.json', '.scss']
     }
+
 };
